d5b063b00e9815f7a5c91aaf3e608ce654b437e0
RodrigoSaporiti <rodrigosaporiti6949@gmail.com>
2026-02-18 23:28:09 -0300

    ..

--- js/index.js
+++ js/index.js
@@ -23,9 +23,13 @@ menu.addEventListener("click",()=>{
 
 const productos = document.getElementById("productos");
 const productosMenu = document.getElementById("productosMenu");
+const back = document.getElementById("back");
 
 
 productos.addEventListener("click", ()=>{
+
+    back.classList.toggle("opacity-0");
+    back.classList.toggle("opacity-100");
 
      
    setTimeout(() => {
@@ -35,8 +39,21 @@ productos.addEventListener("click", ()=>{
 
   }, 10); // 10ms es imperceptible para el humano, pero oro puro para el navegador
 
-    
-    
-   
+})
+
+
+back.addEventListener("click", ()=>{
+
+      setTimeout(() => {
+
+     productosMenu.classList.add("scale-x-0")
+    productosMenu.classList.remove("scale-x-100")
+
+       back.classList.add("opacity-0");
+    back.classList.remove("opacity-100");
+ 
+  }, 10); // 10ms es imperceptible para el humano, pero oro puro para el navegador
+
+
 
 })
